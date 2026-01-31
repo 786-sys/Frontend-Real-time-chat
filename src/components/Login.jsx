a7b44ad489910f1909e121ceaf9fe8171c111ab4
Mohammed Ali <mohammed.sayed16677@sakec.ac.in>
2026-01-31 21:15:26 +0530

    done with some fixes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = ({ Log,loading,setloading, setsigntrue, email, password }) => {
      Log();
   };
   return (
-    <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
+
       <div className="border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg">
         <h2 className="font-medium text-2xl flex justify-between items-center">
           Log in
@@ -67,7 +67,7 @@ const Login = ({ Log,loading,setloading, setsigntrue, email, password }) => {
           </p>
         </div>
       </div>
-    </div>
+  
   );
 };
 
